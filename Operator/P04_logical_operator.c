c3476e8ca533516b9810c244ff4c148889ea9e18
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-01-14 21:27:56 +0530

    Add logical operator examples in P04_logical_operator.c
    Implement logical operators demonstration in C.

---
+++ Operator/P04_logical_operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 0;
+
+    printf("a && b = %d\n", a && b);
+    printf("a || b = %d\n", a || b);
+    printf("!a = %d\n", !a);
+
+    return 0;
+}
+
+/*
+a && b = 0
+a || b = 1
+!a = 0
+*/

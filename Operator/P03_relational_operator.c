be9145915df6ffe8a562218da90c04213e1125a1
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-01-14 21:25:58 +0530

    Add comments for relational operator results
    Add comments explaining the results of relational operations.

---
+++ Operator/P03_relational_operator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 8;
+
+    printf("a < b  = %d\n", a < b);
+    printf("a > b  = %d\n", a > b);
+    printf("a == b = %d\n", a == b);
+    printf("a != b = %d\n", a != b);
+
+    return 0;
+}
+
+  /*
+  a < b  = 1
+  a > b  = 0
+  a == b = 0
+  a != b = 1
+  */

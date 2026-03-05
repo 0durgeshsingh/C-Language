61e141a2fa767f047d4f05c5ecaca1a848a751c0
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-03-05 21:56:26 +0530

    Add switch statement example in C
    Added a switch statement example in C with comments explaining its usage.

---
+++ 14 Switch Statement/Switch.c
@@ -0,0 +1,26 @@
+
+/*
+
+Switch is a control statement in C language used to execute one block of code from multiple options based on the value of a variable or expression.
+It is an alternative to using multiple if-else statements
+
+  */
+
+
+
+switch(expression) {
+    case value1:
+        // code
+        break;
+
+    case value2:
+        // code
+        break;
+
+    case value3:
+        // code
+        break;
+
+    default:
+        // code if no case matches
+}

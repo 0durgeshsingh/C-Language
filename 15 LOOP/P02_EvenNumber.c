10fa2aca4b9af92a6b7fccc87c296e9c5e168832
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-03-09 23:39:17 +0530

    Add program to print even numbers from 2 to 20
    This program prints even numbers from 2 to 20.

---
+++ 15 LOOP/P02_EvenNumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int i = 2;
+
+    while(i <= 20){
+        printf("%d\n", i);
+        i = i + 2;
+    }
+
+    return 0;
+}
+
+/*
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+*/

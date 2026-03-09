4dc33bbf5a8007489cd999a0bf93d2a0ccb156e2
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-03-09 23:33:13 +0530

    Add program to print numbers from 1 to 10
    Implement a program to print numbers from 1 to 10 using a while loop.

---
+++ 15 LOOP/P01_print1to10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    
+    int i = 0;
+    
+    while(i<10){
+        printf("%d\n",i+1);
+        i = i+1;
+    }
+
+    return 0;
+}
+
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*/

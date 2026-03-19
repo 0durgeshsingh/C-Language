0e6175b3f26f7d66b9f582dbf0202878b65bd42c
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-03-19 23:14:02 +0530

    Create P01BasicArray.c

---
+++ 17 Arrays/P01BasicArray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    printf("%d\n", arr[0]); // first element
+    printf("%d\n", arr[4]); // last element
+
+    return 0;
+}

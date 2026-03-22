91c2112147c0058111f1a5b311b7766b8dd87a80
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-03-22 22:01:13 +0530

    Update P01BasicArray.c

--- 17 Arrays/P01BasicArray.c
+++ 17 Arrays/P01BasicArray.c
@@ -4,7 +4,7 @@ int main() {
     int arr[5] = {10, 20, 30, 40, 50};
 
     printf("%d\n", arr[0]); // first element
-    printf("%d\n", arr[4]); // last element
+    printf("%d\n", arr[4]); // last element 
 
     return 0;
 }

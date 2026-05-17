c3fa632bb529ce3589f3ff9b3917803e6d59a61f
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-05-17 23:39:11 +0530

    Update P08_Factorial.c

--- 15 LOOP/P08_Factorial.c
+++ 15 LOOP/P08_Factorial.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int n, i = 1;
+    
+    // Variable to store the input number
+    int n;
+
+    // Counter variable, starts from 1
+    int i = 1;
+
+    // Variable to store factorial result
+    // long is used because factorial values grow very fast
     long factorial = 1;
 
+    // Ask user to enter a number
     printf("Enter number: ");
+
+    // Read the number from user
     scanf("%d", &n);
 
+    // do-while loop executes at least one time
     do {
-        factorial *= i;
-        i++;
-    } while (i <= n);
 
+        // Multiply current value of factorial by i
+        factorial *= i;
+
+        // Increase i by 1
+        i++;
+
+    } while (i <= n); // Continue loop until i becomes greater than n
+
+    // Print the final factorial value
     printf("Factorial = %ld", factorial);
 
-    return 0;
+    return 0; // Program ends successfully
 }
-
-// Output 

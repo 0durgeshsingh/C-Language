28ed2c587d674205e4395542664ddd77662325bd
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-05-18 22:45:28 +0530

    Update P03FloatInput.c

--- 19 UserInupt/P03FloatInput.c
+++ 19 UserInupt/P03FloatInput.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>   // For input and output functions
 
 int main() {
-    float salary;
+
+    float salary;   // float variable to store decimal value
 
     printf("Enter salary: ");
+
     scanf("%f", &salary);
+    // %f is used for float input
+    // &salary stores the value in salary variable
 
     printf("Salary = %.2f", salary);
+    // %.2f prints value with 2 decimal places
 
-    return 0;
+    return 0;   // End of program
 }

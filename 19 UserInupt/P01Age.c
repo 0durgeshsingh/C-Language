ebb30aac5362b514992e675f88e91b7757f554b0
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-05-20 22:54:42 +0530

    Update P01Age.c

--- 19 UserInupt/P01Age.c
+++ 19 UserInupt/P01Age.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include <stdio.h>   // Standard input-output library
 
 int main() {
-    int age;
+    int age;   // Integer variable to store age
 
+    // Ask the user to enter their age
     printf("Enter your age: ");
+
+    // Read integer input from the user
+    // %d is used for integer values
+    // &age gives the memory address where the value will be stored
     scanf("%d", &age);
 
+    // Print the value entered by the user
     printf("You entered: %d\n", age);
 
+    // Return 0 means the program ended successfully
     return 0;
 }

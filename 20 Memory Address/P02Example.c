4ab84fa6825312c79daae002daa5249246fe9aae
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-06-04 23:14:19 +0530

    Add example program for memory address and pointers

---
+++ 20 Memory Address/P02Example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num = 25;
+
+    // Print value of num
+    printf("Value of num = %d\n", num);
+
+    // Print memory address of num
+    printf("Address of num = %p\n", (void *)&num);
+
+    // Declare a pointer and store address of num
+    int *ptr = &num;
+
+    // Print pointer value (address stored in ptr)
+    printf("Value stored in ptr (address) = %p\n", (void *)ptr);
+
+    // Print value at the address stored in ptr
+    printf("Value pointed to by ptr = %d\n", *ptr);
+
+    return 0;
+}

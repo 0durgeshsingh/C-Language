0e4b734fff92a0b301e63fb7c3cdfc83ebb33199
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-07-04 23:23:44 +0530

    Update P4_LargestNumber.c

--- 13 IF ELSE/P4_LargestNumber.c
+++ 13 IF ELSE/P4_LargestNumber.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
+    int a, b, c;                        // Store three numbers
 
     printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);      // Read the numbers
 
+    // Check if 'a' is the largest
     if (a >= b && a >= c) {
         printf("Largest number is %d", a);
     }
+    // Check if 'b' is the largest
     else if (b >= a && b >= c) {
         printf("Largest number is %d", b);
     }
+    // Otherwise, 'c' is the largest
     else {
         printf("Largest number is %d", c);
     }
 
-    return 0;
+    return 0;                           // End program
 }

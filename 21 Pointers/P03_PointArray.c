994322c2b8db8dd8d0c79d1c865a540767ab7a1b
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-07-06 18:42:20 +0530

    Add P03_PointArray.c to demonstrate pointer usage

---
+++ 21 Pointers/P03_PointArray.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+
+    cout << "Array elements using pointer:\n";
+
+    for (int i = 0; i < 5; i++) {
+        cout << *(ptr + i) << " ";
+    }
+
+    return 0;
+}

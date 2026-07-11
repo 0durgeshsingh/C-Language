34de619bbecd6d0ccf8a87cc3ea4f05139dbc48d
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-07-11 18:25:58 +0530

    Add example of implicit type conversion in C
    Demonstrates implicit type conversion from int to float.

---
+++ 09 C Type Conversion/P03_Implicit_type.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num = 25;
+    float value;
+
+    // Implicit conversion
+    value = num;
+
+    printf("Integer = %d\n", num);
+    printf("Float = %.2f\n", value);
+
+    return 0;
+}

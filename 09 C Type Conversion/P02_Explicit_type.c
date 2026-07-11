cb85ec03aa2ece245f8285a754196449a96db5ef
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-07-11 18:24:46 +0530

    Add explicit type conversion in division example
    Implement explicit type conversion for division.

---
+++ 09 C Type Conversion/P02_Explicit_type.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    float result;
+
+    // Explicit type conversion
+    result = (float)a / b;
+
+    printf("Result = %.2f\n", result);
+
+    return 0;
+}

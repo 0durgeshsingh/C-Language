57313af8369023889477de87af4b1342ced471a7
Durgesh Singh <0durgeshsingh.in@gmail.com>
2026-07-18 18:31:57 +0530

    Refactor comments and format in FunctionParameter.c

--- 21 Pointers/P05_FunctionParameter.c
+++ 21 Pointers/P05_FunctionParameter.c
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// Function to square a number using a pointer
 void square(int *n) {
-    *n = (*n) * (*n);
+    *n = (*n) * (*n);   // Square the value
 }
 
 int main() {
-    int num = 6;
+    int num = 6;        // Initialize variable
 
-    square(&num);
+    square(&num);       // Pass address of num
 
-    cout << "Square = " << num;
+    cout << "Square = " << num;   // Print result
 
-    return 0;
+    return 0;           // End program
 }
